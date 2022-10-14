36cd39d329da79aa703add6d6e76b3fe46954d9e
Hasnae <hasnae@viqueen.org>
2022-10-14 19:18:11 +1100

    welcome homebrew git-devbox tap

---
+++ git-devbox.rb
@@ -0,0 +1,18 @@
+class GitDevbox < Formula
+  desc "Collection of useful git extensions that enhance one's local dev environment"
+  homepage "https://github.com/viqueen/git-devbox"
+  url "https://registry.npmjs.org/git-devbox/-/git-devbox-1.0.0.tgz"
+  sha256 "dcd65fdf84784a4a77d41deb704bb0d8a953b592e85a676c2aec3660b7063a2f"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "false"
+  end
+end
