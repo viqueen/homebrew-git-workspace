76c905fb6f715e5244314ced540933084d1f795f
Hasnae <hasnae@viqueen.org>
2023-01-18 11:57:08 +1100

    upgrade to version 3.0.1

--- git-devbox.rb
+++ git-devbox.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GitDevbox < Formula
   desc "Collection of useful git extensions that enhance one's local dev environment"
   homepage "https://github.com/viqueen/git-devbox"
-  url "https://registry.npmjs.org/git-devbox/-/git-devbox-3.0.0.tgz"
-  sha256 "b9cf93d723352cea79ae8d88975373ce9a6085b0079eb03eb283f4fc4d995e82"
+  url "https://registry.npmjs.org/git-devbox/-/git-devbox-3.0.1.tgz"
+  sha256 "74d6d19a0d089530348b164d18c1e82047468995992eb0cde74ec3d63211b692"
   license "Apache-2.0"
 
   depends_on "node"
